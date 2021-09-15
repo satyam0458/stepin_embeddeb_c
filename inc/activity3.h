6632e84e0dc1b0d3587dcc85bd80ae32b82aab45
pavan <89693842+pavankalyanmedishetty@users.noreply.github.com>
2021-09-15 23:44:59 +0530

    Update avtivity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -2,10 +2,10 @@
 #define __ACTIVITY_3_H__
 /**
  * @file activity3.h
- * @author anoop
+ * @author pavan
  * @brief activity3
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-09-15
  * 
  * @copyright Copyright (c) 2021
  * 
