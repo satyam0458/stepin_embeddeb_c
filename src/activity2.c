29e64bd40fc8cf66e53c19c484ed77a0dca3b37a
pavan <89693842+pavankalyanmedishetty@users.noreply.github.com>
2021-09-16 13:35:07 +0530

    Update activity2.c

--- src/activity2.c
+++ src/activity2.c
@@ -1,29 +1,34 @@
+/**
+ * @file Activity2.c
+ * @author pavan
+ * @ADC values 
+ * @version 0.1
+ * @date 2021-09-14
+ * 
+ * @copyright Copyright (c) 2021
+ */
 #include "activity2.h"
 
-/**
- * @brief initializes registers
- * 
- */
-void adc_init()
+void initialiseADC()
 {
-    ADMUX|=(1<<REFS0);
-    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+    ADMUX=(1<<REFS0);
+    ADCSRA= (1<<ADEN)|(7<<ADPS0);
 }
-
-/**
- * @brief converts input from potentiometer to digital value
- * 
- * @param ch 
- * @return uint16_t 
- */
-
-uint16_t adc_read(uint8_t ch)
+uint16_t readadc(uint8_t ch)
 {
     ADMUX&=0xf8;
     ch=ch&0b00000111;
     ADMUX|=ch;
     ADCSRA|=(1<<ADSC);
-    while(!(ADCSRA&(1<<ADIF)));
+    while(!(ADCSRA & (1<<ADIF)));
     ADCSRA|=(1<<ADIF);
-    return (ADC);
+    return(ADC);
+}
+
+uint16_t activity2(void)
+{
+      initialiseADC();
+      uint16_t temp;
+      temp=readadc(0);
+      return temp;
 }
